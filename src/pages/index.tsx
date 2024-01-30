3d95761e0bf37525da70ec9430b490a43c0a863f
Ploy <117616170+yaminP@users.noreply.github.com>
2024-01-30 13:45:53 +0700

    layoout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import RootLayout from "@/components/Layout";
+
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
-    </main>
+    <RootLayout>
+      <div>
+        <h1>Create Link</h1>
+      </div>
+    </RootLayout>
   );
 }
