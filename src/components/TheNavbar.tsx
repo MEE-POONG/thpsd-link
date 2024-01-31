fe8cc598333d8896fe579578461f57e07fecc1f3
devilzeros <devilzeros@outlook.co.th>
2024-01-31 14:37:09 +0700

    est

--- src/components/TheNavbar.tsx
+++ src/components/TheNavbar.tsx
@@ -14,73 +14,92 @@ const TheNavbar: React.FC = () => {
   };
 
   return (
-    <nav className="bg-cyan-500">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
-          {/* Mobile menu button*/}
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700" aria-controls="mobile-menu" aria-expanded="false" onClick={toggleMenu}>
-              <span className="sr-only">Open main menu</span>
-              {/* Icon for menu open */}
-              <svg className={`${isOpen ? 'hidden' : 'block'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-              </svg>
-              {/* Icon for menu close */}
-              <svg className={`${isOpen ? 'block' : 'hidden'} h-6 w-6`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
-          </div>
-          {/* Links section */}
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex-shrink-0 flex items-center">
-              {/* Your Logo Here */}
-              <img
-                className="block lg:hidden h-8 w-auto"
-                src={"https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/7511fb7d-e4d0-4cfb-ac7a-d76528b5f800/250"}
-                alt="Your Logo"
-                style={{ filter: 'invert(100%) brightness(200%)' }}
-              />
+    <nav className="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
+      <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
+        <Link href="#" className="flex items-center">
+          <img src="./images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Landwind</span>
 
-              <img
-                className="hidden lg:block h-8 w-auto"
-                src={"https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/7511fb7d-e4d0-4cfb-ac7a-d76528b5f800/250"}
-                alt="Your Logo"
-                style={{ filter: 'invert(100%) brightness(200%)' }}
-              />
-            </div>
-            <div className="hidden sm:block sm:ml-6">
-              <div className="flex space-x-4">
-                {/* Your navigation links */}
-                {NavDefault.map((item) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className={`text-white hover:underline px-3 py-2 rounded-md text-sm font-bold ${checkPath === item.href ? 'font-bold' : ''}`}
-                  >
-                    {item.name} {item.href === checkPath ? "a" : "b"}
-                  </Link>
-                ))}
-                {/* More links */}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* Mobile menu, show/hide based on menu state */}
-      <div className={`${isOpen ? 'block' : 'hidden'} sm:hidden`} id="mobile-menu">
-        <div className="px-2 pt-2 pb-3 space-y-1">
-          {/* Your mobile navigation links */}
-          {NavDefault.map((item) => (
-            <Link
-              key={item.name}
-              href={item.href}
-              className={`text-white hover:underline px-3 py-2 rounded-md text-sm font-bold ${checkPath === item.href ? 'font-bold' : ''}`}
-            >
-              {item.name}
+        </Link>
+        <div className="flex items-center lg:order-2">
+          <div className="hidden mt-2 mr-4 sm:inline-block">
+            <Link className="github-button" href="https://github.com/themesberg/landwind" data-size="large"
+              data-icon="octicon-star" data-show-count="true"
+              aria-label="Star themesberg/landwind on GitHub">Star
             </Link>
-          ))}
-          {/* More links */}
+          </div>
+          {/* <!-- <Link href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in
+          </Link> --> */}
+          <Link href="https://themesberg.com/product/tailwind-css/landing-page"
+            className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">Download
+          </Link>
+          <button data-collapse-toggle="mobile-menu-2" type="button"
+            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="mobile-menu-2" aria-expanded="false">
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd">
+              </path>
+            </svg>
+            <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clip-rule="evenodd">
+              </path>
+            </svg>
+          </button>
+        </div>
+        <div className="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+          <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            {NavDefault.map((item) => (
+              <li>
+                <Link href={item?.href}
+                  className={`block py-2 pl-3 pr-4 ${checkPath === item.href ? 'text-purple-700' : 'text-gray-700'} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700`}>
+                  {item?.name}
+                </Link>
+              </li>
+            ))}
+
+            <li>
+              <Link href="#"
+                className="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700 lg:p-0 dark:text-white"
+                aria-current="page">Home
+              </Link>
+            </li>
+            <li>
+              <Link href="#"
+                className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                Company
+              </Link>
+            </li>
+            <li>
+              <Link href="#"
+                className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                Marketplace
+              </Link>
+            </li>
+            <li>
+              <Link href="#"
+                className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                Features
+              </Link>
+            </li>
+            <li>
+              <Link href="#"
+                className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                Team
+              </Link>
+            </li>
+            <li>
+              <Link href="#"
+                className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                Contact
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
