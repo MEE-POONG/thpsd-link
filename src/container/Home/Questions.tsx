c8bb76aaac71606517e6a7960a84e8a6d3e2580d
devilzeros <devilzeros@outlook.co.th>
2024-02-01 14:52:42 +0700

    est

--- src/container/Home/Questions.tsx
+++ src/container/Home/Questions.tsx
@@ -23,7 +23,7 @@ const Questions: React.FC = () => {
                                     viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd"
                                         d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                        clip-rule="evenodd">
+                                        clipRule="evenodd">
                                     </path>
                                 </svg>
                             </button>
@@ -54,7 +54,7 @@ const Questions: React.FC = () => {
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd"
                                         d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                        clip-rule="evenodd">
+                                        clipRule="evenodd">
                                     </path>
                                 </svg>
                             </button>
@@ -85,7 +85,7 @@ const Questions: React.FC = () => {
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd"
                                         d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                        clip-rule="evenodd">
+                                        clipRule="evenodd">
                                     </path>
                                 </svg>
                             </button>
@@ -132,7 +132,7 @@ const Questions: React.FC = () => {
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd"
                                         d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                        clip-rule="evenodd">
+                                        clipRule="evenodd">
                                     </path>
                                 </svg>
                             </button>
--- src/container/Home/TitleHead.tsx
+++ src/container/Home/TitleHead.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const TitleHead: React.FC = () => {
     return (
         <section className="bg-white dark:bg-gray-900">
-            <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+            {/* <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
                 <div className="mr-auto place-self-center lg:col-span-7">
                     <h1
                         className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
@@ -36,8 +36,17 @@ const TitleHead: React.FC = () => {
                 <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
                     <img src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/7511fb7d-e4d0-4cfb-ac7a-d76528b5f800/250" alt="hero image" />
                 </div>
-            </div>
+            </div> */}
 
+            <div className="grid grid-cols-3 gap-4">
+                <div className="...">01</div>
+                <div className="...">02</div>
+                <div className="...">03</div>
+                <div className="col-span-2 ...">04</div>
+                <div className="...">05</div>
+                <div className="...">06</div>
+                <div className="col-span-2 ...">07</div>
+            </div>
         </section>
 
     )
