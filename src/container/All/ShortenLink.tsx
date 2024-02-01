d68a8f9a08f4e3c43e53b1ce144dbfceef924867
devilzeros <devilzeros@outlook.co.th>
2024-02-01 11:53:16 +0700

    est

--- src/container/All/ShortenLink.tsx
+++ src/container/All/ShortenLink.tsx
@@ -18,25 +18,33 @@ const ShortenLink: React.FC<LinkShortenerProps> = () => {
     };
     return (
         <section>
-            <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:py-16 lg:pt-28 ">
-                <div className="place-self-center bg-white p-5 rounded-md drop-shadow-lg">
-                    <h1 className=" mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
+            <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:py-16 lg:pt-28 rounded-md drop-shadow-lg">
+                <div className="place-self-center bg-white p-5 ">
+                    <h3 className=" mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-3xl xl:text-4xl dark:text-white">
                         Make every connection count
                         {/* <br /> */}
                         {/* products & brands. */}
-                    </h1>
-                    <h2 className="text-2xl font-bold mb-4 text-center text-blue-600">Shorten a long link</h2>
+                    </h3>
                     <form onSubmit={handleSubmit}>
-                        <div className="mb-4">
-                            <label htmlFor="long-url" className="block text-sm font-medium text-gray-700">Paste a long URL</label>
-                            <input
-                                type="text"
-                                id="long-url"
-                                placeholder="Example: http://super-long-link.com/shorten-it"
-                                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-                                value={longUrl}
-                                onChange={(e) => setLongUrl(e.target.value)}
-                            />
+                        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                            <div className="sm:col-span-4">
+                                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Username
+                                </label>
+                                <div className="mt-2">
+                                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                        <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
+                                        <input
+                                            type="text"
+                                            name="username"
+                                            id="username"
+                                            autoComplete="username"
+                                            className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                            placeholder="janesmith"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div className="mb-4">
                             <label htmlFor="domain" className="block text-sm font-medium text-gray-700">Domain</label>
