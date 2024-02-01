9292d315e1c349791ef1f6c4836a0ebcd11effe8
devilzeros <devilzeros@outlook.co.th>
2024-02-01 14:29:47 +0700

    est

--- src/container/All/ShortenLink.tsx
+++ src/container/All/ShortenLink.tsx
@@ -29,27 +29,17 @@ const ShortenLink: React.FC<LinkShortenerProps> = () => {
                         <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                             <div className="sm:col-span-4">
                                 <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Username
+                                Paste a long URL
                                 </label>
                                 <div className="mt-2">
-                                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                        <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
-                                        <input
-                                            type="text"
-                                            name="username"
-                                            id="username"
-                                            autoComplete="username"
-                                            className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                            placeholder="janesmith"
-                                        />
-                                    </div>
+                                    <input type="text" className="w-full border-b p-1.5 text-base mb-5 focus:outline-none placeholder:italic placeholder:text-xs" placeholder="Example: http://super-long-link.com/shorten-it" />
                                 </div>
                             </div>
                         </div>
                         <div className="mb-4">
                             <label htmlFor="domain" className="block text-sm font-medium text-gray-700">Domain</label>
                             <div className="mt-1 flex rounded-md shadow-sm">
-                                <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">bit.ly</span>
+                                <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">thpsd.com</span>
                                 <input
                                     type="text"
                                     id="domain"
@@ -58,6 +48,7 @@ const ShortenLink: React.FC<LinkShortenerProps> = () => {
                                     value={backHalf}
                                     onChange={(e) => setBackHalf(e.target.value)}
                                 />
+                                {/* ทำปุ่มคัดลอกให้ด้วย */}
                             </div>
                         </div>
                         <div className="mb-6">
