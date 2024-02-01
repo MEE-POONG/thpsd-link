44ef70739b5fa502f4bb30615c64ff008db322c5
Ploy <117616170+yaminP@users.noreply.github.com>
2024-02-01 14:44:01 +0700

    ??

--- src/container/Home/TitleHead.tsx
+++ src/container/Home/TitleHead.tsx
@@ -5,40 +5,34 @@ import React from 'react';
 const TitleHead: React.FC = () => {
     return (
         <section className="bg-white dark:bg-gray-900">
-            <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
-                <div className="mr-auto place-self-center lg:col-span-7">
-                    <h1
-                        className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
-                        Building digital <br />products & brands.
-                    </h1>
-                    <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-                        This
-                        free and open-source landing page template was built using the utility classes from <a
-                            href="https://tailwindcss.com" className="hover:underline">
-                            Tailwind CSS
-                        </a> and based on the
-                        components from the <a href="https://flowbite.com/docs/getting-started/introduction/"
-                            className="hover:underline">
-                            Flowbite Library
-                        </a> and the <a href="https://flowbite.com/blocks/" className="hover:underline">
-                            Blocks System
-                        </a>.
-                    </p>
-                    <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-                        <Link href="/register" className="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-purple-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                            สมัครสมาชิก
-                        </Link>
-                        <Link href="" className="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-purple-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                            ทดลองใช้
-                        </Link>
-                    </div>
-                </div>
-                <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                    <img src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/7511fb7d-e4d0-4cfb-ac7a-d76528b5f800/250" alt="hero image" />
-                </div>
-            </div>
+        <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+          <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">Building digital
+              <br />products & brands.</h1>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">This free and open-source landing page template was built using the utility classes from
+              <Link href="https://tailwindcss.com" className="hover:underline">Tailwind CSS
+              </Link> and based on the components from the
+              <Link href="https://flowbite.com/docs/getting-started/introduction/" className="hover:underline">Flowbite Library
+              </Link> and the
+              <Link href="https://flowbite.com/blocks/" className="hover:underline">Blocks System
+              </Link>.
+            </p>
+            <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+              <Link href="./" className="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                สมัครสมาชิก
 
-        </section>
+              </Link>
+              <Link href="./login" className="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                เข้าสู่ระบบ
+
+              </Link>
+            </div>
+          </div>
+          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <img src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/7511fb7d-e4d0-4cfb-ac7a-d76528b5f800/250" alt="hero image" />
+          </div>
+        </div>
+      </section>
 
     )
 }
