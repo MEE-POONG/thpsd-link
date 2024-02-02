7e26830b413d0e7a9ff874183bbc34ab28eee057
devilzeros <devilzeros@outlook.co.th>
2024-02-02 17:05:31 +0700

    est

--- src/container/Home/TitleHead.tsx
+++ src/container/Home/TitleHead.tsx
@@ -5,6 +5,41 @@ import React from 'react';
 const TitleHead: React.FC = () => {
   return (
     <section className="bg-white dark:bg-gray-900">
+      <div className="space-y-4">
+        <div className="w-96 bg-white shadow rounded">
+          w-96
+        </div>
+        <div className="w-80 bg-white shadow rounded">
+          w-80
+        </div>
+        <div className="w-72 bg-white shadow rounded">
+          w-72
+        </div>
+        <div className="w-64 bg-white shadow rounded">
+          w-64
+        </div>
+        <div className="w-60 bg-white shadow rounded">
+          w-60
+        </div>
+        <div className="w-56 bg-white shadow rounded">
+          w-56
+        </div>
+        <div className="w-52 bg-white shadow rounded">
+          w-52
+        </div>
+        <div className="w-48 bg-white shadow rounded">
+          w-48
+        </div>
+      </div>
+      <div className="grid grid-cols-4 gap-4">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+        <div>04</div>
+        <div>05</div>
+        <div>06</div>
+        <div>09</div>
+      </div>
       <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
         <div className="mr-auto place-self-center lg:col-span-7">
           <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">Building digital
@@ -28,8 +63,8 @@ const TitleHead: React.FC = () => {
             </Link>
           </div>
         </div>
-        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-          <img src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/7511fb7d-e4d0-4cfb-ac7a-d76528b5f800/250" alt="hero image" />
+        <div className="lg:mt-0 lg:col-span-5 lg:flex hidden">
+          <img className='w-full ' src="https://imagedelivery.net/QZ6TuL-3r02W7wQjQrv5DA/7511fb7d-e4d0-4cfb-ac7a-d76528b5f800/250" alt="hero image" />
         </div>
       </div>
     </section>
