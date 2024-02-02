735db7d2da6edf453c03122261dc47eb924994e3
Ploy <117616170+yaminP@users.noreply.github.com>
2024-02-02 16:31:09 +0700

    _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import '@/sass/globals.scss';
+import '@/sass/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
