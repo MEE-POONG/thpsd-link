b5ca19018f6e1ccb6f5449c6d6b24dbddfaa8e4b
devilzeros <devilzeros@outlook.co.th>
2024-02-13 14:07:04 +0700

    est

--- src/components/TheNavbarAcc.tsx
+++ src/components/TheNavbarAcc.tsx
@@ -17,8 +17,7 @@ const TheNavbarAcc: React.FC = () => {
 
   return (
     <header className="z-10 py-4 bg-white shadow-md dark:bg-gray-800">
-      <div
-        className="container flex items-center justify-between h-full px-6 mx-auto text-purple-600 dark:text-purple-300">
+      <div className="container flex items-center justify-between h-full px-6 mx-auto text-purple-600 dark:text-purple-300">
         <button className="p-1 mr-5 -ml-1 rounded-md md:hidden focus:outline-none focus:shadow-outline-purple"
           aria-label="Menu">
           <FaBars />
--- src/container/NotificationDropDown.tsx
+++ src/container/NotificationDropDown.tsx
@@ -1,16 +1,28 @@
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { FaBell, FaCog, FaRegUserCircle, FaSignOutAlt } from 'react-icons/fa';
+import { FaBell, } from 'react-icons/fa';
 
 const NotificationDropDown: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = React.useRef<HTMLDivElement>(null);
 
     const toggleDropdown = () => setIsOpen(!isOpen);
+    // Close the dropdown if the click is outside of it
+    useEffect(() => {
+        const handleClickOutside = (event: any) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        };
 
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [dropdownRef]);
     return (
-        <>
-
+        <div ref={dropdownRef}>
             <button onClick={toggleDropdown} className="relative align-middle rounded-md focus:outline-none focus:shadow-outline-purple" aria-label="Notifications"
                 aria-haspopup="true">
                 <FaBell />
@@ -18,32 +30,36 @@ const NotificationDropDown: React.FC = () => {
                     className="absolute top-0 right-0 inline-block w-3 h-3 transform translate-x-1 -translate-y-1 bg-red-600 border-2 border-white rounded-full dark:border-gray-800"></span>
             </button>
             {isOpen && (
-                <ul className="transition opacity-100 ease-in duration-150 absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:border-gray-700 dark:text-gray-300 dark:bg-gray-700"
-                    aria-label="submenu">
+                <ul className="transition opacity-100 ease-in duration-150 absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:text-gray-300 dark:border-gray-700 dark:bg-gray-700">
                     <li className="flex">
-                        <Link className="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
+                        <Link className="inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
                             href="#">
-                            <FaRegUserCircle className="w-4 h-4 mr-3" />
-                            <span>Profile</span>
+                            <span>Messages</span>
+                            <span
+                                className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-600 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-600">
+                                13
+                            </span>
                         </Link>
                     </li>
                     <li className="flex">
-                        <Link className="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
+                        <Link className="inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
                             href="#">
-                            <FaCog className="w-4 h-4 mr-3" />
-                            <span>Settings</span>
+                            <span>Sales</span>
+                            <span
+                                className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-600 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-600">
+                                2
+                            </span>
                         </Link>
                     </li>
                     <li className="flex">
-                        <Link className="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
+                        <Link className="inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
                             href="#">
-                            <FaSignOutAlt className="w-4 h-4 mr-3" />
-                            <span>Log out</span>
+                            <span>Alerts</span>
                         </Link>
                     </li>
                 </ul>
             )}
-        </>
+        </div>
     );
 };
 
