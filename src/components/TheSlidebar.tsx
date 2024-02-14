e2d87a20fe4daff14640ae4180e7f184305c7881
devilzeros <devilzeros@outlook.co.th>
2024-02-14 17:13:49 +0700

    est

--- src/components/TheSlidebar.tsx
+++ src/components/TheSlidebar.tsx
@@ -21,6 +21,7 @@ const TheSlidebar: React.FC = () => {
         }
     }, []);
 
+
     return (
         <aside className="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-shrink-0">
             <div className="py-4 text-gray-500 dark:text-gray-400">
