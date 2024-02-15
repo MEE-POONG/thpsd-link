de2165a8626bfb3bafb2ed0a28460ed553af15d2
devilzeros <devilzeros@outlook.co.th>
2024-02-15 16:04:45 +0700

    est

--- src/components/TheSlidebar.tsx
+++ src/components/TheSlidebar.tsx
@@ -8,7 +8,7 @@ import MenuLinkList from '@/container/TheSlidebar/MenuLinkList';
 
 const TheSlidebar: React.FC = () => {
     const router = useRouter();
-
+    const isActiveSubPage = (itemHref: string) => router.pathname.startsWith(itemHref);
     return (
         <aside className="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-shrink-0">
             <div className="py-4 text-gray-500 dark:text-gray-400">
@@ -18,15 +18,15 @@ const TheSlidebar: React.FC = () => {
                 <ul className="mt-6">
                     {NavAccount.map((item, index) =>
                         item.head ? (
-                            <li key={index} className={`relative px-6 py-1 text-gray-500 border-l-4 dark:hover:text-gray-200 `}>
+                            <li key={index} className={`relative px-6 py-1 text-gray-500 border-l-4 hover:border-purple-600 hover:text-purple-600 dark:hover:text-gray-200 ${isActiveSubPage(item?.href)? 'text-purple-600 border-purple-600' : ''} `}>
                                 <div className={`inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 `}>
                                     {item.icon}
                                     <span className="ml-4">{item.name}</span>
                                 </div>
-                                <ul className="transition-opacity duration-300 ease-in-out opacity-100 max-h-xl p-2 space-y-2 overflow-hidden text-sm font-medium text-gray-500 rounded-md shadow-inner bg-gray-50 dark:text-gray-400 dark:bg-gray-900">
+                                <ul className="transition-opacity duration-300 ease-in-out opacity-100 max-h-xl p-2 overflow-hidden text-sm font-medium text-gray-500 rounded-md shadow-inner bg-gray-50 dark:text-gray-400 dark:bg-gray-900">
                                     {item.loopPage?.map((subItem, index) => (
                                         <li key={index} className={`relative px-4 py-1 transition-colors duration-150 border-l-4 hover:text-gray-800 dark:hover:text-gray-200 ${router.pathname === item.href + subItem.href ? 'border-l-4 border-purple-600' : 'hover:border-l-4 hover:border-purple-600'}`}>
-                                            <Link href={`${item.href}${subItem.href}`} className="w-full">
+                                            <Link href={`${item.href}${subItem.href}`} className="w-full flex">
                                                 {subItem.name}
                                             </Link>
                                         </li>
